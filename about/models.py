808c1ae379c1f026bde4a4e3b2616b86f6e6cbff
Ivan <petrovic.valpovo@gmail.com>
2024-06-12 21:15:26 +0100

    add image to about

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    profile_image = CloudinaryField("image", default="placeholder" )
 
     def __str__(self):
         return self.title
